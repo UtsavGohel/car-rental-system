db07171bf31cc3053794883e7ddf999b75dae7bb
UtsavGohel <utsavgohel2002@gmail.com>
2025-01-25 20:11:11 +0530

    feat(dashboard): implement basic dashboard component

---
+++ Frontend/src/component/Dashboard.tsx
@@ -0,0 +1,15 @@
+import CardView from "./CardView";
+import CarPackages from "./CarPackages";
+import Navbar from "./Navbar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Navbar />
+      <CarPackages />
+      <CardView />
+    </div>
+  );
+};
+
+export default Dashboard;
