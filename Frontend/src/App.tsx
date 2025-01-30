659bd9277e5276cf9d697f7b0680daaf8d0e0f0d
UtsavGohel <utsavgohel2002@gmail.com>
2025-01-30 20:00:30 +0530

    feat(app): add car-store in provider component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,13 @@
+import { Provider } from "react-redux";
 import Dashboard from "./component/Dashboard";
 import "./index.css";
+import carStore from "./utils/carStore";
 
 function App() {
   return (
-    <>
+    <Provider store={carStore}>
       <Dashboard />
-    </>
+    </Provider>
   );
 }
 
